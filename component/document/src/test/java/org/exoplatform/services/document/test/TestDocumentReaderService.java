b00e640e5056a796bb7b0e8ddea89603f30fa05b
Peter Nedonosko <pnedonosko@exoplatform.com>
2007-11-22 15:53:09 +0000

    tests fix

---
+++ component/document/src/test/java/org/exoplatform/services/document/test/TestDocumentReaderService.java
@@ -0,0 +1,103 @@
+/***************************************************************************
+ * Copyright 2001-2003 The eXo Platform SARL         All rights reserved.  *
+ * Please look at license.txt in info directory for more license detail.   *
+ **************************************************************************/
+package org.exoplatform.services.document.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.services.document.DocumentReaderService;
+import org.exoplatform.test.BasicTestCase;
+/**
+ * Web, Oct 19, 2005 @
+ * @author: Phung Hai Nam
+ * @email: phunghainam@gmail.com
+ */
+public class TestDocumentReaderService extends BasicTestCase {
+
+  DocumentReaderService service_;
+
+  public TestDocumentReaderService(String name) {
+    super(name);
+  }
+
+  public void setUp() throws Exception {
+    PortalContainer pcontainer = PortalContainer.getInstance() ;
+    service_ =
+      (DocumentReaderService) pcontainer.getComponentInstanceOfType(DocumentReaderService.class) ;
+  }
+
+  public void testPDFDocumentReaderService() throws Exception {
+    InputStream is = null;
+    File f = new File("src/test/resources/test.pdf");
+    is = new FileInputStream(f);
+    String text = service_.getDocumentReader("application/pdf").getContentAsText( is);
+    System.out.println(text);
+  }
+
+  public void testWordDocumentReaderService() throws Exception {
+    InputStream is = null;
+    File f = new File("src/test/resources/forum.doc");
+    is = new FileInputStream(f);
+    String text = service_.getDocumentReader("application/msword").getContentAsText(is);
+    System.out.println(text);
+  }
+  
+  public void testWordTemplateReaderService() throws Exception {    
+    File testFile = new File("src/test/resources/test.dot");
+    InputStream inStream = new FileInputStream(testFile);    
+    String text = service_.getDocumentReader("application/msworddot").getContentAsText(inStream);
+    System.out.println("TEXT: " + text);
+  }
+
+  public void testPPTDocumentReaderService() throws Exception {
+    InputStream is = null;
+    File f = new File("src/test/resources/test.ppt");
+    is = new FileInputStream(f);
+    String text = service_.getDocumentReader("application/powerpoint").getContentAsText(is);
+    System.out.println(text);
+  }
+
+  public void testExcelDocumentReaderService() throws Exception {
+    InputStream is = null;
+    File f = new File("src/test/resources/test.xls");
+    is = new FileInputStream(f);
+    String text = service_.getDocumentReader("application/excel").getContentAsText(is);
+    System.out.println(text);
+  }
+
+  public void testHTMLDocumentReaderService() throws Exception {
+    InputStream is = null;
+    File f = new File("src/test/resources/test.html");
+    is = new FileInputStream(f);
+    String text = service_.getDocumentReader("text/html").getContentAsText(is);
+    System.out.println(text);
+  }
+
+  public void testXMLDocumentReaderService() throws Exception {
+    InputStream is = null;
+    File f = new File("src/test/resources/test.xml");
+    is = new FileInputStream(f);
+    String text = service_.getDocumentReader("text/xml").getContentAsText(is);
+    System.out.println(text);
+  }
+
+  public void testTextPlainDocumentReaderService() throws Exception {
+    InputStream is = null;
+    File f = new File("src/test/resources/test.txt");
+    is = new FileInputStream(f);
+    String text = service_.getDocumentReader("text/plain").getContentAsText(is);
+    System.out.println(text);
+  }
+
+  public void testOODocumentReaderService() throws Exception {
+    File f = new File("src/test/resources/subscription.odt");
+    InputStream is = new FileInputStream(f);
+    String text = service_.getDocumentReader("application/vnd.oasis.opendocument.text").getContentAsText( is);
+    System.out.println(text);
+  }
+
+}
