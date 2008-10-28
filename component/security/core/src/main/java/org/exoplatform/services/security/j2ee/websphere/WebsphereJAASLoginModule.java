2bfcc6cf3cc1e7467715dd4d5e575b2ab861c491
Alexey Zavizionov <alexey.zavizionov@exoplatform.com>
2008-10-28 11:22:05 +0000

    COR-86  org.exoplatform.services.organization.ext.websphere have to move in security

--- component/security/core/src/main/java/org/exoplatform/services/security/j2ee/websphere/WebsphereJAASLoginModule.java
+++ component/security/core/src/main/java/org/exoplatform/services/security/j2ee/websphere/WebsphereJAASLoginModule.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see<http://www.gnu.org/licenses/>.
  */
-package org.exoplatform.services.security.ext.websphere;
+package org.exoplatform.services.security.j2ee.websphere;
 
 import java.security.Principal;
 import java.util.ArrayList;
