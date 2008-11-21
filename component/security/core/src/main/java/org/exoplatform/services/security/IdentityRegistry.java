02ef5c3cc77193d6b9898ca64bcdc877127891ee
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2008-11-21 14:33:48 +0000

    COR-96 : Replace HashMap by ConcurrentHashMap

--- component/security/core/src/main/java/org/exoplatform/services/security/IdentityRegistry.java
+++ component/security/core/src/main/java/org/exoplatform/services/security/IdentityRegistry.java
@@ -17,7 +17,11 @@
 
 package org.exoplatform.services.security;
 
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.commons.logging.Log;
+import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.services.log.ExoLogger;
 
 /**
  * Created by The eXo Platform SAS .
@@ -28,25 +32,87 @@ import java.util.HashMap;
 
 public class IdentityRegistry {
 
-  private HashMap<String, Identity> identities = new HashMap<String, Identity>();
+  /**
+   * "concurrency-level".
+   */
+  public static final String                        INIT_PARAM_CONCURRENCY_LEVEL = "concurrency-level";
 
-  public IdentityRegistry() {
+  /**
+   * Logger.
+   */
+  private static final Log                          LOG                          = ExoLogger.getLogger(IdentityRegistry.class.getName());
 
+  /**
+   * Default concurrency level.
+   */
+  private static final int                           DEFAULT_CONCURRENCY_LEVEL    = 16;
+
+  /**
+   * Identities.
+   */
+  private final ConcurrentHashMap<String, Identity> identities;
+
+  public IdentityRegistry(InitParams params) {
+    this(parseConcurrencyLevel(params));
   }
 
+  /**
+   * Try to parse concurrency level attribute from init parameters.
+   * 
+   * @param params See {@link InitParams}
+   * @return parsed parameter or default if parameter not set or can't be parsed
+   */
+  private static int parseConcurrencyLevel(InitParams params) {
+    try {
+      return Integer.valueOf(params.getValueParam(INIT_PARAM_CONCURRENCY_LEVEL).getValue());
+    } catch (NullPointerException e) {
+      LOG.warn("Parameter " + INIT_PARAM_CONCURRENCY_LEVEL
+          + " was not found in configuration, default " + DEFAULT_CONCURRENCY_LEVEL + "will be used.");
+      return DEFAULT_CONCURRENCY_LEVEL;
+    } catch (Exception e) {
+      LOG.error("Can't parse parameter " + INIT_PARAM_CONCURRENCY_LEVEL, e);
+      return DEFAULT_CONCURRENCY_LEVEL;
+    }
+  }
+
+  /**
+   * Create identity registry.
+   * @param concurrencyLevel concurrency level for ConcurrentHashMap 
+   */
+  private IdentityRegistry(int concurrencyLevel) {
+    identities = new ConcurrentHashMap<String, Identity>(concurrencyLevel, 0.75f, concurrencyLevel);
+  }
+
+  /**
+   * Get identity for supplied user ID.
+   * @param userId user ID
+   * @return identity or null if not found
+   */
   public Identity getIdentity(String userId) {
     return identities.get(userId);
   }
 
+  /**
+   * Register new identity in registry.
+   * @param identity {@link Identity}
+   */
   public void register(Identity identity) {
     this.identities.put(identity.getUserId(), identity);
   }
 
+  /**
+   * Remove identity with supplied user ID. 
+   * @param userId user ID
+   */
   public void unregister(String userId) {
     this.identities.remove(userId);
   }
 
+  /**
+   * Remove all identities.
+   */
   void clear() {
     identities.clear();
   }
+
 }
