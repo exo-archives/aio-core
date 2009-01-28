8b27894597f502a75e485f5a4bfffa736f339ef3
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2009-01-28 17:05:19 +0000

    WS-COR-109 : change artifact jcifs:jcifs:1.12.17 to org.samba.jcifs:jcifs:1.12.19

--- component/security/ntlm/test-jaas.sh
+++ component/security/ntlm/test-jaas.sh
@@ -4,9 +4,9 @@
 export MAVEN_REPO=$HOME/exo/exo-dependencies/repository
 
 $JAVA_HOME/bin/java -cp $MAVEN_REPO/org/exoplatform/core/exo.core.component.security.core/2.1.4-SNAPSHOT/exo.core.component.security.core-2.1.4-SNAPSHOT.jar:\
-$MAVEN_REPO/org/exoplatform/kernel/exo.kernel.component.common/2.0.5-SNAPSHOT/exo.kernel.component.common-2.0.5-SNAPSHOT.jar:\
+$MAVEN_REPO/org/exoplatform/kernel/exo.kernel.component.common/2.0.7-SNAPSHOT/exo.kernel.component.common-2.0.7-SNAPSHOT.jar:\
 $MAVEN_REPO/commons-logging/commons-logging/1.1/commons-logging-1.1.jar:\
-$MAVEN_REPO/jcifs/jcifs/1.2.17/jcifs-1.2.17.jar:\
+$MAVEN_REPO/org/samba/jcifs/jcifs/1.2.19/jcifs-1.2.19.jar:\
 target/classes:\
 target/test-classes:. \
 org.exoplatform.services.security.ntlm.NTLMAuthenticatorTest $1 $2
